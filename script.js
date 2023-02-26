c65ac46143604a688037ac7134e66d777f2065f0
Zaid <the1zaid2ahmad3@gmail.com>
2023-02-26 19:09:39 +0530

    Make random computer choice

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function getComputerChoice(){
+
+    let randomNum = Math.floor(Math.random()*3)
+
+    if(randomNum === 0){
+        return "Rock"
+    }else if(randomNum === 1){
+        return "Paper"
+    }else{
+        return "Scissor"
+    }
+    
+}
+
+console.log(getComputerChoice())
