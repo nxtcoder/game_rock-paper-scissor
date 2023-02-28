707fa1f745454211e44b2097604646d73c142a09
Zaid <the1zaid2ahmad3@gmail.com>
2023-03-01 00:01:49 +0530

    make playable rock paper scissor

--- script.js
+++ script.js
@@ -4,13 +4,42 @@ function getComputerChoice(){
     let randomNum = Math.floor(Math.random()*3)
 
     if(randomNum === 0){
-        return "Rock"
+        return "ROCK"
     }else if(randomNum === 1){
-        return "Paper"
+        return "PAPER"
     }else{
-        return "Scissor"
+        return "SCISSOR"
     }
     
 }
 
-console.log(getComputerChoice())
+function playRound(player, computer){
+    if (player === computer){
+    return "Draw"
+    }else if (player === "ROCK" && computer === "PAPER"){
+    return ("Loose")
+    }else if(player === "PAPER" && computer === "SCISSOR"){
+    return ("Loose")
+    }else if(player === "SCISSOR" && computer === "ROCK"){
+    return ("Loose")
+    }else {
+    return "Win"
+    }
+}
+
+function game(){
+    let computerSelection = getComputerChoice();
+    let playerInput = prompt("Your choice?").toUpperCase()
+    console.log(playRound(playerInput, computerSelection))
+    console.log("PLayer's Choice - " + playerInput)
+    console.log("Computer's Choice - " + computerSelection)
+}
+
+function playMatch(){
+    for(let i = 1; i <= 5; i++){
+        game()
+        console.log("Round - " + i )
+        }
+}
+
+playMatch()
